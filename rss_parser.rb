a6e7cfde722673349357562cc4e82d9a895df935
Mikee Pourhadi <mikeepourhadi@gmail.com>
2014-09-07 17:53:30 -0500

    functional RSSparse superclass

--- rss_parser.rb
+++ rss_parser.rb
@@ -2,21 +2,36 @@ require 'rss'
 require 'open-uri'
 require 'nokogiri'
 
-rss_feed = "http://www.npr.org/rss/rss.php?id=1003"
+class RSSParser
+	attr_reader :feed_link, :links_to_articles
+	def initialize(feed_link)
+		@feed_link = feed_link
+		@rss_content = ""
+		@links_to_articles = []
+		@parsed_rss = nil
+	end
 
-rss_content = ""
+	def parse_rss_feed
+		open(feed_link) do |f|
+   		@rss_content = f.read
+		end
+		@parsed_rss = RSS::Parser.parse(@rss_content, false)
+	end
 
-open(rss_feed) do |f|
-   rss_content = f.read
+	def retrieve_article_links
+		@parsed_rss.items.each { |item| @links_to_articles << item.link }
+	end
+
+	def parse_article_text 
+		links_to_articles.each do |article_link|
+			doc = Nokogiri::HTML(open(article_link))
+			puts doc.css('#storytext p').methods
+		end
+	end
+
 end
 
-rss = RSS::Parser.parse(rss_content, false)
-
-links = []
-
-rss.items.each { |item| links << item.link }
-
-links.each do |article_link|
-	doc = Nokogiri::HTML(open(article_link))
-	puts doc.css('#storytext p')
-end
+rss = RSSParser.new("http://www.npr.org/rss/rss.php?id=1003")
+rss.parse_rss_feed
+rss.retrieve_article_links
+rss.parse_article_text
