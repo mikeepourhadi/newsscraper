cbef9fa1a0111dbea4888e78cd5602cab5bfbbff
Mikee Pourhadi <mikeepourhadi@gmail.com>
2014-09-07 17:41:38 -0500

    add parse method for NPR site, working nokogiri

--- rss_parser.rb
+++ rss_parser.rb
@@ -10,13 +10,7 @@ open(rss_feed) do |f|
    rss_content = f.read
 end
 
-# Parse the feed, dumping its contents to rss
 rss = RSS::Parser.parse(rss_content, false)
-
-# Output the feed title and website URL
-puts "Title: #{rss.channel.title}"
-puts "RSS URL: #{rss.channel.link}"
-puts "Total entries: #{rss.items.size}"
 
 links = []
 
